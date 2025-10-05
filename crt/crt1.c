7fafafe944ebf5a348b22088d51e40ece5517fd7
biosfood <leisenhau@gmail.com>
2025-10-05 22:50:50 +0200

    remove argument mocking

--- crt/crt1.c
+++ crt/crt1.c
@@ -11,10 +11,8 @@ weak void _fini();
 int __libc_start_main(int (*)(), int, char **,
 	void (*)(), void(*)(), void(*)());
 
-hidden void _start_c(long *_p)
+hidden void _start_c(long *p)
 {
-    (void)_p;
-    long p[] = {0, 0, 0, 0, 0};
 	int argc = p[0];
 	char **argv = (void *)(p+1);
 	__libc_start_main(main, argc, argv, _init, _fini, 0);
